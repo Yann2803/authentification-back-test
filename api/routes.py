5a968cfdd9c259dcab8ab81410dd611cc611fd3a
yann <yann@emotion-tech.com>
2021-07-07 11:24:48 +0200

    secret key authentification ok

--- api/routes.py
+++ api/routes.py
@@ -1,19 +1,57 @@
-from flask import Blueprint , request ,jsonify
-from flask_jwt_extended import create_access_token
-from flask_jwt_extended import get_jwt_identity
-from flask_jwt_extended import jwt_required
+from flask import Flask, Blueprint, request, jsonify
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token, create_refresh_token,
+    get_jwt_identity
+)
+from datetime import timedelta
+from flask_cors import CORS, cross_origin
+import sys
+import os
 
-from flask_cors import CORS
 
+app = Flask(__name__)
+jwt = JWTManager(app)
 api = Blueprint('account_api', __name__)
 CORS(api)
 
-@api.route('/login', methods=['POST'])
+
+@api.route('/login', methods=['GET', 'POST'])
+@cross_origin()
 def create_token():
     email = request.json.get("email", None)
     password = request.json.get("password", None)
-    if email != "test" or password != "test":
+    if email != "test@test.com" or password != "test":
         return jsonify({"msg": "Bad username or password"}), 401
 
-    access_token = create_access_token(identity=email)
-    return jsonify(access_token=access_token)
+    access_token = create_access_token(identity=email, fresh=True)
+    refresh_token = create_refresh_token(identity=email)
+    return jsonify(access_token=access_token, refresh_token=refresh_token)
+
+
+@api.route("/refresh", methods=['GET', 'POST'])
+@jwt_required(refresh=True)
+def refresh():
+    print("HELLO")
+    sys.stdout.flush()
+    get_token = request.headers.get('Authorization')
+    print("this is the refresh_token", get_token)
+    sys.stdout.flush()
+    identity = get_jwt_identity()
+    print("this is the identity", identity)
+    sys.stdout.flush()
+    new_token = create_access_token(identity=identity, fresh=False)
+    response = jsonify(access_token=new_token)
+    print("this is the response", response)
+    sys.stdout.flush()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    print("this is the response", response)
+    sys.stdout.flush()
+    return response, 200
+
+
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
--- app.py
+++ app.py
@@ -1,16 +1,27 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from api.routes import api
 from flask_jwt_extended import JWTManager
+from datetime import timedelta
+import os
 
 app = Flask(__name__)
-
+app.config["SECRET_KEY"] = os.getenv('MY_SECRET')
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
 app.register_blueprint(api)
-app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+app.config['PROPAGATE_EXCEPTIONS'] = True
 jwt = JWTManager(app)
 
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    headers = request.headers
+    auth = headers.get("X-Api-Key")
+    if auth == 'super-secret':
+        return jsonify({"message": "OK: Authorized"}), 200
+    else:
+        return jsonify({"message": "ERROR: Unauthorized"}), 401
+
 
 if __name__ == "__main__":
     app.run()
